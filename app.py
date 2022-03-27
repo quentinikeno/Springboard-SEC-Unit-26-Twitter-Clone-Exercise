d393e50a0e2f682bf88313a60251d8c8adc950b6
Quentin Ikeno <ikenoq@gmail.com>
2022-03-27 13:30:08 -1000

    Set intercept redirects to false

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
