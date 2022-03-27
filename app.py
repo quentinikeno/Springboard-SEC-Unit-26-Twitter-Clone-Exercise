75f59203822edb7a5b6d40752a03940979f200b4
Quentin Ikeno <ikenoq@gmail.com>
2022-03-27 13:24:37 -1000

    Fix logout

--- app.py
+++ app.py
@@ -113,7 +113,10 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+    
+    flash("You've been logged out!", "success")
+    return redirect("/login")
 
 
 ##############################################################################
