fab4db6f41d199e4cb33ad5271da020ebd49f59d
Quentin Ikeno <ikenoq@gmail.com>
2022-03-27 20:39:57 -1000

    Show link to likes on user detail page

--- app.py
+++ app.py
@@ -155,6 +155,7 @@ def users_show(user_id):
                 .order_by(Message.timestamp.desc())
                 .limit(100)
                 .all())
+    
     return render_template('users/show.html', user=user, messages=messages)
 
 
